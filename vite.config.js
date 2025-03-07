832c76ea75d90a751be469efef0a550b88d186aa
Jinane <jinanealayli@gmail.com>
2025-03-07 14:22:48 +0200

    test  chunkSize

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite"
+import react from "@vitejs/plugin-react"
+ 
+export default defineConfig(({ mode }) => {
+  
+  const env = loadEnv(mode, process.cwd(), "")
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+  return {
+    plugins: [react()],
+    build: {
+     
+      chunkSizeWarningLimit: 3000, 
+    },
+    server: {
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:5000",
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  }
 })
